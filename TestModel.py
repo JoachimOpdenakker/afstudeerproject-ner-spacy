d65bae951b1e4a53e3b51e49789fb8646d3f7143
WouterJ76 <wouter.janssen@student.ucll.be>
2022-02-21 18:08:13 +0100

    visualisatie test results

--- TestModel.py
+++ TestModel.py
@@ -1,9 +1,13 @@
 import spacy
+from spacy import displacy
 
 test_text = "I ate Sushi yesterday. Maggi is a common fast food."
 # nlp = spacy.load("./output/model-best")
 nlp = spacy.load("./output/model-last")
 doc = nlp(test_text)
+colors = {"FOOD": "linear-gradient(90deg, #aa9cfc, #fc9ce7)"}
+options = {"ents": ["FOOD"], "colors": colors}
+displacy.serve(doc, style="ent", options=options)
 print("Entities in '%s'" % test_text)
 for ent in doc.ents:
   print(ent)
