16646e651ff341bb9d2425b43d60d413844e5c92
Joachim Opdenakker <joachim@opdenakker.email>
2022-02-25 15:45:42 +0100

    display some more sane results

--- TestModel.py
+++ TestModel.py
@@ -16,14 +16,21 @@ with open('500ksample-europefilter-address.csv', newline="\n") as csvfile:
 nlp = spacy.load("./output/model-best")
 # nlp = spacy.load("./output/model-last")
 list = []
-for i in test_text:
+correct_counter = 0
+total_counter = 0
+for i in test_text:    
     doc = nlp(i)
-    list.append(doc)
+    if len(doc.ents) == 4:
+        total_counter += 1
+        if len(i) == len(str(doc.ents[0]) + str(doc.ents[1]) + str(doc.ents[2]) + str(doc.ents[3])) + 3:
+            list.append(doc)
+            correct_counter += 1
     # for token in doc:
     #     print(str(token) + ";")
 colors = {"STREET": "linear-gradient(90deg, #aa9cfc, #fc9ce7)", "NUMBER": "linear-gradient(90deg, #3f5efb, #fc466b)", "ZIPCODE": "linear-gradient(90deg, #090979, #00d4ff)", "CITY": "linear-gradient(90deg, #eeaeca, #94bbe9)", "OTHER": "linear-gradient(90deg, #22c1c3, #fdbb2d)",}
 options = {"ents": ["CITY", "STREET", "NUMBER", "ZIPCODE", "OTHER"], "colors": colors}
-
+print("correctness", correct_counter/total_counter)
+print("total", total_counter)
 displacy.serve(list, style="ent", options=options)
 # print("Entities in '%s'" % test_text)
 # for ent in doc.ents:
