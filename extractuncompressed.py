e593a58f3bd6f205873fe42b960eb6e331e32986
Joachim Opdenakker <joachim@opdenakker.email>
2022-02-25 15:46:54 +0100

    try some filters on data to see if we can force some stuff in our dataset

--- extractuncompressed.py
+++ extractuncompressed.py
@@ -18,17 +18,24 @@ df = pd.read_pickle("500ksample.pkl")
 # # print(pf.groupby(['person_ctry_code']).mean())
 # pf.to_csv('./street.csv', index=False)
 
-
+extract_street = df.filter(items=['street', 'city', 'zip_code', 'person_ctry_code'])
+dropstreet = extract_street.dropna()
+filterstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+nostreet = filterstreet[filterstreet['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]
+# dropstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+# newstreet = dropstreet[dropstreet['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]]
 # pf = pf.groupby(['person_ctry_code'])
-pf = df.filter(items=['address_1', 'address_2', 'person_ctry_code'])
-pf = pf.dropna()
-pf = pf.sort_values(by=[('person_ctry_code')])
-pf = pf[pf['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]
-pf = pf[pf['address_1'].str.match('[0-9]+( )?[a-zA-Z]+|[a-zA-Z]+( )?[0-9]+')]
+bla = df.filter(items=['address_1', 'address_2', 'person_ctry_code'])
+pf = bla.dropna()
+test = pf.sort_values(by=[('person_ctry_code')])
+probeersel = test[test['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]
+# pf = pf[pf['address_1'].str.match('[0-9]+( )?[a-zA-Z]+|[a-zA-Z]+( )?[0-9]+')]
+result = probeersel.filter(items=['address_1', 'address_2'])
 
 
 
-
-pf.to_csv('./address.csv', index=False)
+result.to_csv('./500ksample-europefilter-address.csv', index=False)
 
 # bla.close()
+
+#%%
