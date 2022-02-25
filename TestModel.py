183c1419ddb455f8b6823ab05fdffb4226c79e1c
WouterJ76 <wouter.janssen@student.ucll.be>
2022-02-25 12:21:15 +0100

    TestModel and changes to config

--- TestModel.py
+++ TestModel.py
@@ -1,13 +1,31 @@
+import csv
 import spacy
+from spacy.tokens import DocBin
 from spacy import displacy
 
-test_text = "Keilalahdentie 4,02150 Espoo\n20 Park Crescent,London W1N 4AL\nWeisshsusstrasse 2 52066, Aachen\n14,rue Royale 75008 Paris\n32 Wakelin Avenue,Sawston, Cambridgeshire CB2 4DA\nHüninger Strasse 25,14195 Berlin"
+# test_text = ["Keilalahdentie 4,02150 Espoo", "Weisshsusstrasse 2,52066 Aachen", "14 rue Royale,75008 Paris", "Hüninger Strasse 25,14195 Berlin", "19 bis rue Hoche,49100 Angers"]
+test_text = []
+with open('address.csv', newline="\n") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        row = ','.join(row)
+        test_text.append(row)
+
+# print(test_text)
+
 nlp = spacy.load("./output/model-best")
 # nlp = spacy.load("./output/model-last")
-doc = nlp(test_text)
+list = []
+for i in test_text:
+    doc = nlp(i)
+    list.append(doc)
+    # for token in doc:
+    #     print(str(token) + ";")
+    # db.add(doc)
 colors = {"STREET": "linear-gradient(90deg, #aa9cfc, #fc9ce7)", "NUMBER": "linear-gradient(90deg, #3f5efb, #fc466b)", "ZIPCODE": "linear-gradient(90deg, #090979, #00d4ff)", "CITY": "linear-gradient(90deg, #eeaeca, #94bbe9)", "OTHER": "linear-gradient(90deg, #22c1c3, #fdbb2d)",}
 options = {"ents": ["CITY", "STREET", "NUMBER", "ZIPCODE", "OTHER"], "colors": colors}
-displacy.serve(doc, style="ent", options=options)
+
+displacy.serve(list, style="ent", options=options)
 # print("Entities in '%s'" % test_text)
 # for ent in doc.ents:
 #   print(ent)
