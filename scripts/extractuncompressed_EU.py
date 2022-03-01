0db490796d501f52b612340ce722c7a1ae88cf48
sbeWolti76 <woltidecoolste@gmail.com>
2022-03-01 11:02:54 +0100

    EU extract and USA extract

---
+++ scripts/extractuncompressed_EU.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+# pickle_file =  pickle.load(open("100ksample.pkl", 'rb'))
+# bla = open("blacombo.txt", "a")
+
+# print(pickle_file)
+
+df = pd.read_pickle("../data/samples/500ksample.pkl")
+
+# print(df.count())
+
+# df.filter(items=['street', 'city'])
+
+# print(df.filter(items=['street', 'city']))
+
+# pf = df.filter(items=['street', 'city', 'zip_code', 'person_ctry_code'])
+# pf = pf.dropna()
+# # print(pf.groupby(['person_ctry_code']).mean())
+# pf.to_csv('./street.csv', index=False)
+
+# extract_street = df.filter(items=['street', 'city', 'zip_code', 'person_ctry_code'])
+# dropstreet = extract_street.dropna()
+# filterstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+# nostreet = filterstreet[filterstreet['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]
+# dropstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+# newstreet = dropstreet[dropstreet['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]]
+# pf = pf.groupby(['person_ctry_code'])
+pf = df.filter(items=['address_1', 'address_2', 'person_ctry_code'])
+pf = pf.dropna()
+pf = pf.sort_values(by=[('person_ctry_code')])
+pf = pf[pf['person_ctry_code'].isin(['AT','BE','BG','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IT','LT','LU','LV','MT','NL','PO','PT','RO','SE','SI','SK'])]
+pf = pf[pf['address_1'].str.match('[0-9]+( )?[a-zA-Z]+|[a-zA-Z]+( )?[0-9]+')]
+result = pf.filter(items=['address_1', 'address_2'])
+
+
+
+
+result.to_csv('../data/samples/500ksample-europefilter-address.csv', index=False)
+
+# bla.close()
+
+#%%
---
+++ scripts/extractuncompressed_USA.py
@@ -0,0 +1,32 @@
+import pickle
+import pandas as pd
+# pickle_file =  pickle.load(open("100ksample.pkl", 'rb'))
+# bla = open("blacombo.txt", "a")
+
+# print(pickle_file)
+
+df = pd.read_pickle("../data/samples/500ksample.pkl")
+
+#STREET CITY ZIPCODE
+# extract_street = df.filter(items=['street', 'city', 'zip_code', 'person_ctry_code'])
+# dropstreet = extract_street.dropna()
+# filterstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+# nostreet = filterstreet[filterstreet['person_ctry_code'].isin(['US'])]
+# dropstreet = dropstreet.sort_values(by=[('person_ctry_code')])
+# newstreet = dropstreet[dropstreet['person_ctry_code'].isin(['US'])]]
+# pf = pf.groupby(['person_ctry_code'])
+
+#ADDRESS1 ADDRESS2
+pf = df.filter(items=['address_1', 'address_2', 'person_ctry_code'])
+pf = pf.dropna()
+pf = pf.sort_values(by=[('person_ctry_code')])
+pf = pf[pf['person_ctry_code'].isin(['US'])]
+# pf = pf[pf['address_1'].str.match('[0-9]+( )?[a-zA-Z]+|[a-zA-Z]+( )?[0-9]+')]
+result = pf.filter(items=['address_1', 'address_2'])
+
+
+
+
+result.to_csv('../data/samples/500ksample-americanfilter-address.csv', index=False)
+
+#%%
