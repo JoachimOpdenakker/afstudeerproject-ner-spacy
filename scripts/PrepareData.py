166e31d8bab4e2f68561331b3341352f3ec98b80
sbeWolti76 <woltidecoolste@gmail.com>
2022-03-01 11:04:38 +0100

    changes to eu generate spacy

--- scripts/PrepareData.py
+++ scripts/PrepareData.py
@@ -4,7 +4,7 @@ from spacy.tokens import DocBin
 sys.path.insert(0, '../data/trainingData')
 from training import TRAININGS_DATA as TD
 
-f = open("problems.txt", 'w')
+f = open("./output/problems.txt", 'w')
 f.close()
 
 
@@ -12,7 +12,7 @@ nlp = spacy.blank("en")
 
 infixes = nlp.Defaults.infixes + [r'\,']
 nlp.tokenizer.infix_finditer = spacy.util.compile_infix_regex(infixes).finditer
-
+counter = 0
 db = DocBin()
 for text, annotations in TD:
     doc = nlp(text)
@@ -22,14 +22,16 @@ for text, annotations in TD:
     for start, end, label in annotations:
         span = doc.char_span(start, end, label=label)
         if span is None:
-            f = open("problems.txt", 'a')
+            f = open("./output/problems.txt", 'a')
             f.write("Problem:" + doc.text + " (" + doc.text[start:end] + ")\n")
             f.close()
         else:
             ents.append(span)
-    # print(ents)
-    doc.ents = ents
-    db.add(doc)
+    if len(ents) == 4:
+        counter += 1
+        doc.ents = ents
+        db.add(doc)
+print(counter)
 db.to_disk("../data/binaryData/train.spacy")
 
 db = DocBin()
@@ -39,13 +41,11 @@ for text, annotations in TD:
     for start, end, label in annotations:
         # print(doc.text[start:end])
         span = doc.char_span(start, end, label=label)
-        if span is None:
-            print("Problem:" + doc.text + " (" + doc.text[start:end] + ")" )
-        else:
+        if span is not None:
             ents.append(span)
-    # print(ents)
-    doc.ents = ents
-    db.add(doc)
+    if len(doc.ents) == 4:
+        doc.ents = ents
+        db.add(doc)
 db.to_disk("../data/binaryData/dev.spacy")
 
 
--- scripts/generate-spacy-format.py
+++ scripts/generate-spacy-format.py
@@ -1,136 +1,153 @@
 import csv
 import re
+import getopt, sys
 from unidecode import unidecode
-with open('../data/samples/500ksample-europefilter-address.csv', newline="\n") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    counter = 0 
-    f = open("./output/fouten.txt", 'w')
-    f.close
-    f = open("../data/trainingData/training.py", 'w')
-    f.write("TRAININGS_DATA = [\n")
-    f.close()
-    counter = 0
-    fail_numberstreet = 0
-    fail_cityzip = 0 
-    for row in csvreader:
-        correct = True
-        counter += 1
-        # print(row)
-        # exit()
-        counter +=1
-        # print("counter: " + str(counter))
-        adres = ','.join(row)
-        adres = unidecode(adres)
-        # print(adres)
-        # adres = adres.replace(",", ";")
-        # if re.match(".*,.*,.*", adres):
-            # print(adres)
-            # print("FUCK YOU")
-        # else:
-        if not re.match(".*,.*,.*", adres):
-            # print("counter: " + str(counter))
-            adreslengte = len(adres)
-            straat = row[0]
-            totale_lengte_straat = len(straat)
-            city = row[1]
-            straatposbegin = ""
-            straatposeind = ""
-            nummerposbegin =""
-            nummerposeind = ""
-            cityposbegin = ""
-            cityposeind = ""
-            zipcodeposbegin =""
-            zipcodeposeind =""
-            # nummer + straat
-            # oud: ([0-9]+[A-Z]*)( )?([a-zA-Z]+)
-            # nieuw: (^[0-9]+.*([\u00C0-\u017Fa-zA-Z]+)),
-            # probeersel: ^([0-9]+)( .*)*( )([\u00C0-\u017Fa-zA-Z]+)
-            if re.match('^([0-9\-]+( ?bis)?) ([ \u00C0-\u017Fa-zA-Z\'\-]+)', straat):
-                split = re.search('^([0-9\-]+( ?bis)?) ([ \u00C0-\u017Fa-zA-Z\'\-]+)', straat)
-                splitstraat = split.group(3)
-                splitnummer = split.group(1)
-                straatlengte = len(splitstraat)
-                nummerlengte = len(splitnummer)
-                nummerposbegin = 0
-                nummerposeind = nummerlengte
-                straatposbegin = nummerposeind + 1
-                straatposeind = straatposbegin + straatlengte
 
-            # straat + nummer
-            # oude: ([a-zA-Z]+)( )?([0-9]+)
-            # nieuw: (([a-zA-Z]+) ([0-9]+.*,))
-            # oude nieuwe: ^([ \u00C0-\u017Fa-zA-Z\']+) ([0-9]+( ?bis)?)
-            elif re.match('^([ \u00C0-\u017Fa-zA-Z\'ü\-]+) ([0-9\/\-]+( ?[a-zA-Z]*)?)', straat):
-                split = re.search('^([ \u00C0-\u017Fa-zA-Z\'ü\-]+) ([0-9\/\-]+( ?[a-zA-Z]*)?)', straat)
-                splitstraat = split.group(1)
-                splitnummer = split.group(2)
-                nummerlengte = len(splitnummer)
-                straatlengte = len(splitstraat)
-                straatposbegin = 0
-                straatposeind = straatlengte
-                nummerposbegin = straatlengte + 1
-                nummerposeind = nummerposbegin + nummerlengte
-            else:
-                f = open("fouten.txt", 'a')
-                f.write("street: " + adres + "\n")
+argumentList = sys.argv[1:]
+
+options = "eu"
+
+long_options = ["Europe", "USA"]
+
+try:
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    if arguments == []:
+        print("Use : -e, -u, --Europe, --USA")
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-e", "--Europe"):
+            with open('../data/samples/500ksample-europefilter-address.csv', newline="\n") as csvfile:
+                csvreader = csv.reader(csvfile, delimiter=',')
+                counter = 0 
+                f = open("./output/fouten.txt", 'w')
+                f.close
+                f = open("../data/trainingData/training.py", 'w')
+                f.write("TRAININGS_DATA = [\n")
                 f.close()
-                fail_numberstreet +=1
-                correct = False
-            
-            # zipcode + city
-            # oud: ([A-Z]*-?[0-9]+)( )?([\u00C0-\u017Fa-zA-Z]+)
-            # nieuw: ([ A-Z-]*[0-9]+) ?([\u00C0-\u017Fa-zA-Z .\/-]+)$
-            if re.match('([ A-Z-]*[0-9]+( ?[A-Z]{2})?) ?([\u00C0-\u0338a-zA-Z ./-]+)$', city):
-                split = re.search('([ A-Z-]*[0-9]+( ?[A-Z]{2})?) ?([\u00C0-\u0338a-zA-Z ./-]+)$', city)
-                splitcity = split.group(3)
-                splitzipcode = split.group(1)
-                # print("city: " + splitcity)
-                # print("zipcode: " + splitzipcode)
-                citylengte = len(splitcity)
-                zipcodelengte = len(splitzipcode)
-                zipcodeposbegin = totale_lengte_straat + 1
-                zipcodeposeind = zipcodeposbegin + zipcodelengte
-                cityposbegin = zipcodeposeind + 1
-                cityposeind = cityposbegin + citylengte
+                counter = 0
+                fail_numberstreet = 0
+                fail_cityzip = 0 
+                for row in csvreader:
+                    correct = True
+                    counter += 1
+                    # print(row)
+                    # exit()
+                    counter +=1
+                    # print("counter: " + str(counter))
+                    adres = ','.join(row)
+                    adres = unidecode(adres)
+                    # print(adres)
+                    # adres = adres.replace(",", ";")
+                    # if re.match(".*,.*,.*", adres):
+                        # print(adres)
+                        # print("FUCK YOU")
+                    # else:
+                    if not re.match(".*,.*,.*", adres):
+                        # print("counter: " + str(counter))
+                        adreslengte = len(adres)
+                        adres1 = row[0]
+                        totale_lengte_straat = len(adres1) + 1
+                        adres2 = row[1]
+                        # nummer + straat
+                        # oud: ([0-9]+[A-Z]*)( )?([a-zA-Z]+)
+                        # nieuw: (^[0-9]+.*([\u00C0-\u017Fa-zA-Z]+)),
+                        # probeersel: ^([0-9]+)( .*)*( )([\u00C0-\u017Fa-zA-Z]+)
+                        if re.match('^([0-9\-]+( ?bis)?) ([ \u00C0-\u017Fa-zA-Z\'\-]+)', adres1):
+                            split = re.search('^([0-9\-]+( ?bis)?) ([ \u00C0-\u017Fa-zA-Z\'\-]+)', adres1)
+                            print(split.span(3))
+                            straat = split.span(3)
+                            nummer = split.span(1)
+                            # splitstraat = split.group(3)
+                            # splitnummer = split.group(1)
+                            # straatlengte = len(splitstraat)
+                            # nummerlengte = len(splitnummer)
+                            # nummerposbegin = 0
+                            # nummerposeind = nummerlengte
+                            # straatposbegin = nummerposeind + 1
+                            # straatposeind = straatposbegin + straatlengte
 
-            # city + zipcode 
-            elif re.match('([\u00C0-\u017Fa-zA-Z ./-]+) ?([ A-Z-]*[0-9]+)$', city):
-                split = re.search('([\u00C0-\u017Fa-zA-Z ./-]+) ?([ A-Z-]*[0-9]+)$', city)
-                splitcity = split.group(1)
-                splitzipcode = split.group(2)
-                # print("city: " + splitcity)
-                # print("zipcode: " + splitzipcode)
-                zipcodelengte = len(splitzipcode)
-                citylengte = len(splitcity)
-                cityposbegin = totale_lengte_straat + 1
-                cityposeind = cityposbegin + citylengte
-                zipcodeposbegin = cityposeind + 1
-                zipcodeposeind = zipcodeposbegin + zipcodelengte
-            else:
-                f = open("fouten.txt", 'a')
-                f.write("city: " + adres + "\n")
+                        # straat + nummer
+                        # oude: ([a-zA-Z]+)( )?([0-9]+)
+                        # nieuw: (([a-zA-Z]+) ([0-9]+.*,))
+                        # oude nieuwe: ^([ \u00C0-\u017Fa-zA-Z\']+) ([0-9]+( ?bis)?)
+                        elif re.match('^([ \u00C0-\u017Fa-zA-Z\'\-]+) ([0-9\/\-]+( ?[a-zA-Z]*)?)', adres1):
+                            split = re.search('^([ \u00C0-\u017Fa-zA-Z\'\-]+) ([0-9\/\-]+( ?[a-zA-Z]*)?)', adres1)
+                            straat = split.span(1)
+                            nummer = split.span(2)
+                            # splitstraat = split.group(1)
+                            # splitnummer = split.group(2)
+                            # nummerlengte = len(splitnummer)
+                            # straatlengte = len(splitstraat)
+                            # straatposbegin = 0
+                            # straatposeind = straatlengte
+                            # nummerposbegin = straatlengte + 1
+                            # nummerposeind = nummerposbegin + nummerlengte
+                        else:
+                            f = open("fouten.txt", 'a')
+                            f.write("street: " + adres + "\n")
+                            f.close()
+                            fail_numberstreet +=1
+                            correct = False
+                        
+                        # zipcode + city
+                        # oud: ([A-Z]*-?[0-9]+)( )?([\u00C0-\u017Fa-zA-Z]+)
+                        # nieuw: ([ A-Z-]*[0-9]+) ?([\u00C0-\u017Fa-zA-Z .\/-]+)$
+                        if re.match('([ A-Z-]*[0-9]+( ?[A-Z]{2})?) ?([\u00C0-\u0338a-zA-Z ./-]+)$', adres2):
+                            split = re.search('([ A-Z-]*[0-9]+( ?[A-Z]{2})?) ?([\u00C0-\u0338a-zA-Z ./-]+)$', adres2)
+                            zipcode = list(split.span(1))
+                            city = list(split.span(3))
+                            zipcode[0] += totale_lengte_straat
+                            zipcode[1] += totale_lengte_straat
+                            city[0] += totale_lengte_straat
+                            city[1] += totale_lengte_straat
+                            # splitcity = split.group(3)
+                            # splitzipcode = split.group(1)
+                            # print("city: " + splitcity)
+                            # print("zipcode: " + splitzipcode)
+                            # citylengte = len(splitcity)
+                            # zipcodelengte = len(splitzipcode)
+                            # zipcodeposbegin = totale_lengte_straat + 1
+                            # zipcodeposeind = zipcodeposbegin + zipcodelengte
+                            # cityposbegin = zipcodeposeind + 1
+                            # cityposeind = cityposbegin + citylengte
+
+                        # city + zipcode 
+                        elif re.match('([\u00C0-\u017Fa-zA-Z ./-]+) ?([ A-Z-]*[0-9]+)$', adres2):
+                            split = re.search('([\u00C0-\u017Fa-zA-Z ./-]+) ?([ A-Z-]*[0-9]+)$', adres2)
+                            zipcode = list(split.span(2))
+                            city = list(split.span(1))
+                            zipcode[0] += totale_lengte_straat
+                            zipcode[1] += totale_lengte_straat
+                            city[0] += totale_lengte_straat
+                            city[1] += totale_lengte_straat
+                            # splitcity = split.group(1)
+                            # splitzipcode = split.group(2)
+                            # # print("city: " + splitcity)
+                            # # print("zipcode: " + splitzipcode)
+                            # zipcodelengte = len(splitzipcode)
+                            # citylengte = len(splitcity)
+                            # cityposbegin = totale_lengte_straat + 1
+                            # cityposeind = cityposbegin + citylengte
+                            # zipcodeposbegin = cityposeind + 1
+                            # zipcodeposeind = zipcodeposbegin + zipcodelengte
+                        else:
+                            f = open("fouten.txt", 'a')
+                            f.write("city: " + adres + "\n")
+                            f.close()
+                            fail_cityzip += 1
+                            correct = False
+
+                        if correct:
+                            f = open("../data/trainingData/training.py", 'a')
+                            f.write("\t(\""+adres+"\","+"[("+str(straat[0])+","+str(straat[1])+", \"STREET\"),("+str(nummer[0])+","+str(nummer[1])+", \"NUMBER\"),("+str(city[0])+","+str(city[1])+", \"CITY\"),("+str(zipcode[0])+","+str(zipcode[1])+", \"ZIPCODE\")]),\n")
+                            f.close()
+                        # print("(\""+adres+"\","+"[("+str(straatposbegin)+","+str(straatposeind)+", \"STREET\"),("+str(nummerposbegin)+","+str(nummerposeind)+", \"NUMBER\"),("+str(cityposbegin)+","+str(cityposeind)+", \"CITY\"),("+str(zipcodeposbegin)+","+str(zipcodeposeind)+", \"ZIPCODE\")])")
+                f = open("../data/trainingData/training.py", "a")
+                f.write("]")
                 f.close()
-                fail_cityzip += 1
-                correct = False
+                print("failed on street: ", fail_numberstreet)
+                print("failed on city: ", fail_cityzip)
+        elif currentArgument in ("-u", "--USA"):
+            print("USA")
 
-
-
-
-        # city = row[1]
-        # citylengte = len(city)
-        # straatposbegin = 0
-        # straatposeind = straatlengte
-        # cityposbegin = straatlengte+1
-        # cityposeind = adreslengte
-        # # print(row[0] + "length: " + str(len(row[0])))
-            if correct:
-                f = open("../data/trainingData/training.py", 'a')
-                f.write("\t(\""+adres+"\","+"[("+str(straatposbegin)+","+str(straatposeind)+", \"STREET\"),("+str(nummerposbegin)+","+str(nummerposeind)+", \"NUMBER\"),("+str(cityposbegin)+","+str(cityposeind)+", \"CITY\"),("+str(zipcodeposbegin)+","+str(zipcodeposeind)+", \"ZIPCODE\")]),\n")
-                f.close()
-            # print("(\""+adres+"\","+"[("+str(straatposbegin)+","+str(straatposeind)+", \"STREET\"),("+str(nummerposbegin)+","+str(nummerposeind)+", \"NUMBER\"),("+str(cityposbegin)+","+str(cityposeind)+", \"CITY\"),("+str(zipcodeposbegin)+","+str(zipcodeposeind)+", \"ZIPCODE\")])")
-    f = open("../data/trainingData/training.py", "a")
-    f.write("]")
-    f.close()
-    print("failed on street: ", fail_numberstreet)
-    print("failed on city: ", fail_cityzip)
-        # ("Reuterstr. 131,53113 Bonn", [(0, 10, "STREET"), (11, 14, "NUMBER"), (15, 20, "ZIPCODE"), (21, 25, "CITY")],
+except getopt.error as err:
+    print("Use : -e, -u, --Europe, --USA")
